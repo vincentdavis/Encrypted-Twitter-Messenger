eee104a32653bec7e7826b346f879239e677f815
Yogesh Dwivedi <yogesh.p@cisinlabs.com>
2016-01-06 22:22:06 +0530

    Add kivy app for twitter

---
+++ src/kivy_twitter.py
@@ -0,0 +1,56 @@
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.properties import StringProperty
+from kivy.clock import Clock
+from kivy.uix.textinput import TextInput
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.relativelayout import RelativeLayout
+from twython import Twython
+import netcheck
+from twitter.twitter import *
+
+
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+class TweetButton(Button):
+    def on_press(self):
+        def print_request(success, msg):
+            if success:
+                Logger.info('Application notified that tweet succeeded')
+            else:
+                Logger.info('Application notified that tweet failed')
+        twitter = AndroidTwitter()
+        twitter.tweet(app.ttext.text, print_request)
+
+
+class TwitterApp(App):
+    def __init__(self, *args, **kwargs):
+        global app
+        app = self
+        super(TwitterApp, self).__init__(*args, **kwargs)
+
+    def on_start(self):
+        twitter = AndroidTwitter()
+        twitter.home()
+
+
+    def build(self):
+        self.ttext = TextInput(text='Kivy will set you free!',
+                                 size_hint=(1.0, 0.3),
+                                 font_size=18)
+        tb = TweetButton(text='Tweet Text',
+                         size_hint=(0.5, 0.2))
+        root = StackLayout()
+        root.add_widget(self.ttext)
+        root.add_widget(tb)
+        return root
+
+
+if __name__ == '__main__':
+    TwitterApp().run()
---
+++ src/twitter/twitter.py
@@ -0,0 +1,64 @@
+import requests
+from twython import Twython
+
+CALLBACK_URL= 'kivy://'
+MAX_ATTEMPTS = 3 # steps to try again on exception
+SAVE_PATH = './twitter_credentials.json'
+MAX_DIMENSION = 375 # for image tweets
+APP_KEY = ''
+APP_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
+
+
+class Request():
+    def __init__(self, payload, callback=None):
+        self.payload = payload
+        if callback is not None:
+            self.callback = callback
+        else:
+            self.callback = lambda *args, **kwargs: None
+
+class AndroidTwitter():
+    ''' Twitter implementation that uses OAuth and Twitter API'''
+
+    def get_twitter_instance(self):
+        twitter = Twython(APP_KEY,APP_SECRET)
+        auth = twitter.get_authentication_tokens()
+        OAUTH_TOKEN = auth['oauth_token']
+        OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+        twitter = Twython(
+                APP_KEY,
+                APP_SECRET,
+                OAUTH_TOKEN,
+                OAUTH_TOKEN_SECRET
+                )
+        return twitter
+
+    def tweet(self, status='test', callback=None):
+        twitter = self.get_twitter_instance()
+        twitter.update_status(status=status)
+        return true
+
+    def home(self):
+        twitter = Twython(APP_KEY,APP_SECRET)
+        auth = twitter.get_authentication_tokens()
+        OAUTH_TOKEN = auth['oauth_token']
+        OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+        auth_url = auth['auth_url']
+        return auth_url
+        
+
+
+    def _new_request(self, request):
+        self.request = request
+        if not self._processing:
+            self._tries = MAX_ATTEMPTS
+            self._processing = True
+            self._error_msg = 'There was a problem sending the tweet'
+            self._process_request()
+            self._toast('Connecting', False)
+            return True
+        self._toast('Tweet Already in Progress')
+        return False
+
